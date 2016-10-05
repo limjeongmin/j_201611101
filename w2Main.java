c7a1c0889b1a17f7cde43f1b6ff32e2b69231991
limjeongmin <jml8740@naver.com>
2016-10-05 13:36:34 +0900

    사각형 면적구하기

---
+++ w2Main.java
@@ -0,0 +1,33 @@
+﻿public class Rectangle {
+private int width;
+private int height;
+public Rectangle(int width, int height) {
+this.width = width;
+this.height = height;
+}
+public int getWidth() {
+return width;
+}
+public int getHeight() {
+return height;
+}
+public void setWidth(int newWidth) {
+width=newWidth;
+}
+public void setHeight(int newHeight) {
+height=newHeight;
+}
+public void setSize(int w, int h) {
+setWidth(w);
+setHeight(h);
+}
+public int calcArea() {
+return width*height;
+}
+}
+
+Rectangle r = new Rectangle(10,10);
+r.calcArea();
+
+r.setSize(10,11w);
+r.calcArea();
