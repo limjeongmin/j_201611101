397c136fca0314f0a7918c1a11b3e4414fba9734
limjeongmin <jml8740@naver.com>
2016-10-26 00:21:27 +0900

    Add files via upload

---
+++ TurtleDemoMain.java
@@ -0,0 +1,14 @@
+﻿package com.sd.turtle; 
+import ch.aplu.turtle.*; 
+class TurtleDemoMain{ 
+	Turtle t1=new Turtle(); 
+	TurtleDemoMain(){ 
+		for(int i=0;i<4;i++){ 
+			t1.forward(100); 
+			t1.left(90); 
+}	 
+} 
+	public static void main(String[] args){ 
+		new TurtleDemoMain(); 
+} 
+} 
