909d6b81374f2044477783504465f117e10a7a0d
limjeongmin <jml8740@naver.com>
2016-11-02 11:59:55 +0900

    바람개비

---
+++ MakeSwirlsSquare.java
@@ -0,0 +1,24 @@
+﻿import ch.aplu.turtle.*; 
+ 
+ 
+public class MakeSwirlsSquare{ 
+ 
+ 
+ Turtle t1 = new Turtle(); 
+  
+ void makeSwirlsSquare(int size,int bigger,int turns,double angle){  
+     for(int i=0;i<turns;i++){  
+         if(i%2!=0){  
+              size+=bigger;  
+              t1.forward(size);  
+              t1.right(angle); 
+         } else {   
+              t1.fd(size);  
+              t1.right(angle); 
+  } 
+ } 
+ }  
+ public static void main(String[] args){   
+  new MakeSwirlsSquare().makeSwirlsSquare(10,15,25,90.0); 
+ }  
+}  
