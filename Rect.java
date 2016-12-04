9419bef5ae76bdf6530146f0a8bad34a5a2934ce
limjeongmin <jml8740@naver.com>
2016-12-04 23:50:58 +0900

    상속 -삼각형

---
+++ Rect.java
@@ -0,0 +1,35 @@
+package com.sd.ch8;
+
+public class Rect extends Shape{  			
+	private double width;  			
+	private double height;
+	private double area;
+	private double perimeter;
+
+	public Rect(double w, double h){  			
+		this.width=w;  			
+		this.height=h;
+		this.area= w*h;
+		this.perimeter=2*(w+h);
+	}
+
+	public double calcArea(){  			
+		return this.area;  			
+	}  			
+	public double calcPerimeter(){  			
+		return this.perimeter;  			
+	}
+	
+	public String toString(){
+		return "Rect.." + super.toString();
+	}
+	
+	public static void main(Stirng[] args){
+		Rect r=new Rect(20.0,25.0);
+		System.out.println("Area is" + r.calcAre());
+		System.out.println("Perimeter is" + r.calcPerimeter());
+		System.out.print(r.toString());
+	}
+}
+}	
+			
