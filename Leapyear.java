64f2271d85ac6e6a584e42f4e824e92417dcd102
limjeongmin <jml8740@naver.com>
2016-12-21 17:43:19 +0900

    Update Leapyear.java

--- Leapyear.java
+++ Leapyear.java
@@ -1,17 +1,14 @@
 class LeapYear{
-	public void leapyear(int[] year){
+	public void leapYear(int[] year){
 		for(int y:year){
-			if (((y % 4 == 0) && (y % 100 != 0)) || (y % 400 == 0)) {
-				System.out.printf("%d is leap year\n", y);
-			}
+			if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0) {
+				System.out.printf("%d is leap year\n"+ y);								
+			} 
+			else{		
+				System.out.println("%d is not leapyear"+y);
+			}		
 		}
-	}
-	public void leapyear(int year){
-		if (((y % 4 == 0) && (y % 100 != 0)) || (y % 400 == 0)) {
-			System.out.printf("%d is leap year\n", year);
-		}
-	}
-	
+	}		
 	public static void main(String[] args){
 		int[] year={1869, 1997, 2002, 2016, 1711};
 		LeapYear ly=new LeapYear();
